dc510e52a78edc690bc0ecac57650af903a5b682
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 10:09:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-dream-dev-57208",
+  url: "https://super-dream-dev-57208.botics.co",
+  api: "https://super-dream-dev-57208.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
